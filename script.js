680d92587bbc4cfe092810e12bf022dfebfb0a8d
Anne <steamengine.a@gmail.com>
2023-07-13 12:23:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 30) {
+    if (scores[activePlayer] >= 20) {
       // Finish the game
       playing = false;
       diceElement.classList.add('hidden');
